8aa5337d3995af774ade4f3aa0797a13f21dfde3
Marcsllite <marcsllite@gmail.com>
2024-09-11 10:59:38 -0400

    reverting changes to os.name system property, build

--- src/test/java/com/marcsllite/util/OSUtilTest.java
+++ src/test/java/com/marcsllite/util/OSUtilTest.java
@@ -1,11 +1,19 @@
 package com.marcsllite.util;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OSUtilTest {
+    private static final String ACTUAL_OS = System.getProperty("os.name");
+
+    @AfterAll
+    public static void afterAll() {
+        System.setProperty("os.name", ACTUAL_OS);
+    }
+
     @ParameterizedTest(name = "os.name = {0}, getOs = {1}")
     @MethodSource("getOs_data")
     void testGetOS(String osName, OSUtil.OS expectedOs) {
