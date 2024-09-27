fff58e4b3438731db8bc820434eadec5706f27f0
Marcsllite <marcsllite@gmail.com>
2024-09-27 11:12:46 -0400

    updating formatting

--- src/main/java/com/marcsllite/util/handler/ImageHandler.java
+++ src/main/java/com/marcsllite/util/handler/ImageHandler.java
@@ -23,15 +23,25 @@ public final class ImageHandler {
 
   private ImageHandler() {}
 
-  public static Image getErrorImage() { return new Image("/images/error.png"); }
+  public static Image getErrorImage() {
+      return new Image("/images/error.png");
+  }
   
-  public static Image getSuccessImage() { return new Image("/images/success.png"); }
+  public static Image getSuccessImage() {
+      return new Image("/images/success.png");
+  }
   
-  public static Image getMinusImage() { return new Image("/images/minus.png"); }
+  public static Image getMinusImage() {
+      return new Image("/images/minus.png");
+  }
   
-  public static Image getColorLogoBkgImage() { return new Image("/images/color_UMass_logo_background.png"); }
+  public static Image getColorLogoBkgImage() {
+      return new Image("/images/color_UMass_logo_background.png");
+  }
   
-  public static String getColorLogoBkgPath() { return  System.getProperty("user.dir") + File.separator + "color_UMass_logo_background.png"; }
+  public static String getColorLogoBkgPath() {
+      return  System.getProperty("user.dir") + File.separator + "color_UMass_logo_background.png";
+  }
   
   public static Image getShipmentImage(AppColor color)  {
       return switch (color) {
